9d860b849e1db67c98299fdead24e2f903d83dbe
samphillips123 <sphillips123@gmail.com>
2023-12-04 17:54:23 -0800

    updated data to use first and last name separately to allow for better sorting later.

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,9 +1,9 @@
 const Member = ({ member }) => {
     return (
         <div>
-            {/* <img src={member.headShotImg.fields.file.url} alt={`Headshot of ${member.fullName}.`} /> */}
+            {/* <img src={member.headShotImg.fields.file.url} alt={`Headshot of ${member.firstName} ${member.lastName}.`} /> */}
             <img src={`https://flagsapi.com/${member.homeCountryCode}/shiny/48.png`} alt={`Flag representing team members' home country of ${member.homeCountry}`} />
-            <h3>{member.fullName}</h3>
+            <h3>{member.firstName} {member.lastName}</h3>
         </div>
     )
 }
