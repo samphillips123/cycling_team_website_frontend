f957897a9fb9344becb495923925122817a46dd9
samphillips123 <sphillips123@gmail.com>
2023-12-05 15:56:32 -0800

    added nav link for home as the team name/logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,13 @@ const Header = (props) => {
     return (
         <div>
             <nav>
-                <a href='/team/'>Team</a>
-                <a href='/partners/'>Partners</a>
-                <a href='/calendar/'>Calendar</a>
-                <a href='/racingnews/'>Racing</a>
-                <a href='/news/'>News</a>
+                <a href='/' id='teamHomeNav'>TeamName</a> 
+                {/* The link for home will be replaced with a variable and rendered to the left of the header */}
+                <a href='/team/' class='pageLinks'>Team</a>
+                <a href='/partners/' class='pageLinks'>Partners</a>
+                <a href='/calendar/' class='pageLinks'>Calendar</a>
+                <a href='/racingnews/' class='pageLinks'>Racing</a>
+                <a href='/news/' class='pageLinks'>News</a>
             </nav>
         </div>
     )
