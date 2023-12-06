d450503f9c4730dcc36e2069def340fa1bb8b5a1
samphillips123 <sphillips123@gmail.com>
2023-12-05 17:05:40 -0800

    st up basic social media links with placeholder url in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,29 @@
+// IMPORT SOCIAL MEDIA ICONS
+import facebookLogo24 from '../public/imgs/facebook-logo-24.png'
+import instagramLogo24 from '../public/imgs/instagram-logo-24.png'
+import twitterLogo24 from '../public/imgs/twitter-logo-24.png'
+import youtubeLogo24 from '../public/imgs/youtube-logo-24.png'
+
 const Footer = (props) => {
     return (
-        <h1>Footer</h1>
+        <div>
+            <nav>
+                <a href='/' id='teamHomeNav'>TeamName</a>
+                {/* The link for home will be replaced with a variable and rendered to the left of the header */}
+                <a href='https://www.instagram.com/'>
+                    <img src={instagramLogo24} alt='Instagram icon with link.' />
+                </a>
+                <a href='https://twitter.com/'>
+                    <img src={twitterLogo24} alt='Twitter icon with link.' />
+                </a>
+                <a href='https://www.facebook.com/'>
+                    <img src={facebookLogo24} alt='Facebook icon with link.' />
+                </a>
+                <a href='https://www.youtube.com/'>
+                    <img src={youtubeLogo24} alt='YouTube icon with link.' />
+                </a>
+            </nav>
+        </div>
     )
 }
 
