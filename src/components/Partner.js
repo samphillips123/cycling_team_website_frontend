e2b247263919db2a291d0ed4bd475b46d427faa6
samphillips123 <sphillips123@gmail.com>
2023-12-06 18:24:11 -0800

    switched the onclick to an anonymous function

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -1,7 +1,7 @@
 const Partner = ({ partner }) => {
     return (
         <div>
-            <img src={partner.partnerLogo.fields.file.url} alt={`Logo of team partner, ${partner.partnerName}`} />
+            {/* <img src={partner.partnerLogo.fields.file.url} alt={`Logo of team partner, ${partner.partnerName}`} /> */}
             <h3>{partner.partnerName}</h3>
         </div>
     )
