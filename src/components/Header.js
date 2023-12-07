48c7cac31f9be5a884b9aa378c97801b8214c18e
samphillips123 <sphillips123@gmail.com>
2023-12-06 19:01:01 -0800

    updated onClick

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,19 +8,19 @@ const Header = ({ mainContent, setCollection }) => {
                 <Link to={'/'}>
                     {mainContent.map((team) => <TeamLogo team={team} key={team._id} />)}
                 </Link>
-                <Link to={'/team/'} onClick={() =>setCollection('team')} >
+                <Link to={'/team/'} onClick={() => setCollection('team')} >
                     <h3>Team</h3>
                 </Link>
-                <Link to={'/partners/'} onClick={() =>setCollection('partners')} >
+                <Link to={'/partners/'} onClick={() => setCollection('partners')} >
                     <h3>Partners</h3>
                 </Link>
-                <Link to={'/calendar/'} onClick={() =>setCollection('calendarEvents')} >
+                <Link to={'/calendar/'} onClick={() => setCollection('calendarEvents')} >
                     <h3>Calendar</h3>
                 </Link>
-                <Link to={'/racingnews/'} onClick={() =>setCollection('teamNews')} >
+                <Link to={'/racingnews/'} onClick={() => setCollection('teamNews')} >
                     <h3>Racing</h3>
                 </Link>
-                <Link to={'/news/'} onClick={() =>setCollection('teamNews')} >
+                <Link to={'/news/'} onClick={() => setCollection('teamNews')} >
                     <h3>News</h3>
                 </Link>
             </nav>
