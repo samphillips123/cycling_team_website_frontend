ea9c9e2cc50f9e0db449322740ed3db1a303805d
samphillips123 <sphillips123@gmail.com>
2023-12-12 18:47:37 -0800

    made footer dynamic with screen size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,43 @@
 import { Link } from 'react-router-dom'
 import TeamLogo from './TeamLogo'
 import TeamSocials from './TeamSocials'
+import { useMediaQuery } from '../utils/useMediaQuery';
 
 const Footer = (props) => {
+    // media query hook
+    const isDesktop = useMediaQuery('(min-width: 1024px)');
+
     return (
-        <div className='flex items-center justify-between'>
-            <Link to='/' className='m-20' >
-                {props.mainContent.map((team) => <TeamLogo team={team} key={team._id} />)}
-            </Link>
-            <div className='shrink-0'>
-                {props.mainContent.map((socials) => <TeamSocials socials={socials} key={socials._id} />)}
-            </div>
-        </div>
+        <>
+            {
+                isDesktop && (
+                    <>
+                        <div className='flex items-center justify-between'>
+                            <Link to='/' className='m-20 shrink-0' >
+                                {props.mainContent.map((team) => <TeamLogo team={team} key={team._id} />)}
+                            </Link>
+                            <div className='shrink-0'>
+                                {props.mainContent.map((socials) => <TeamSocials socials={socials} key={socials._id} />)}
+                            </div>
+                        </div>
+                    </>
+                )
+            }
+            {
+                !isDesktop && (
+                    <>
+                        <div className='flex flex-col items-center'>
+                            <Link to='/' className='mt-20 shrink-0' >
+                                {props.mainContent.map((team) => <TeamLogo team={team} key={team._id} />)}
+                            </Link>
+                            <div className='shrink-0'>
+                                {props.mainContent.map((socials) => <TeamSocials socials={socials} key={socials._id} />)}
+                            </div>
+                        </div>
+                    </>
+                )
+            }
+        </>
     )
 }
 
