11043b1151f707bad28a964c5e8fb1c6088b36ac
samphillips123 <sphillips123@gmail.com>
2023-12-12 16:36:08 -0800

    installed tailwindcss framer-motion react-scroll and aos

--- src/App.js
+++ src/App.js
@@ -191,6 +191,8 @@ function App() {
 
   return (
     <div className="App">
+      <h1 className='text-3xl font-bold underline'> Hello world! </h1>
+
       <Header
         mainContent={mainContent}
       />
