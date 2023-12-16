b3471efc55ed6ec794ce5910629de5e059ac4953
samphillips123 <sphillips123@gmail.com>
2023-12-15 17:56:08 -0800

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,27 @@
 const About = (props) => {
     return (
         <div className='flex flex-col justify-center max-w-2xl m-20 '>
-            <h2 className='text-lg font-bold m-4 mt-10'>About Ritchey Velocio Racing</h2>
-            <p className='text-base'>Welcome to Ritchey Velocio Racing, a powerhouse in the world of professional cycling, fueled by the unwavering support of our title sponsors, Ritchey Logic and Velocio Apparel. As a dynamic and ambitious team, we embody the essence of speed, precision, and style, mirroring the values of our esteemed sponsors.</p>
-            <h2 className='text-lg font-bold m-4'>Our Team: Powered by Passion</h2>
-            <p className='text-base'>Ritchey Velocio Racing is more than just a cycling team; we are a collective of passionate athletes, skilled technicians, and dedicated support staff united by a common goal  achieving excellence in the world of professional cycling. Our roster of elite cyclists represents a diverse array of talents and backgrounds, coming together to form a formidable force on the race circuit. With a shared commitment to hard work, perseverance, and sportsmanship, we proudly carry the Ritchey Velocio Racing banner across the globe.</p>
-            <h2 className='text-lg font-bold m-4'>Join the Journey</h2>
-            <p className='text-base'>Whether you're a cycling enthusiast, a fan of the sport, or a potential partner seeking to align with a winning team, we invite you to join us on this thrilling journey. Ritchey Velocio Racing is not just a team; it's a community of individuals dedicated to pushing the boundaries of what is possible in the world of competitive cycling. Together with Ritchey Logic and Velocio Apparel, we are shaping the future of the sport, one race at a time.</p>
-            <h3 className='text-lg font-semibold mt-4'>Thank you for being part of the Ritchey Velocio Racing experience. Saddle up, and let's ride to victory!</h3>
+            <h2 className='text-lg font-bold m-4 mt-10'>
+                About Ritchey Velocio Racing
+            </h2>
+            <p className='text-base'>
+                Welcome to Ritchey Velocio Racing, a powerhouse in the world of professional cycling, fueled by the unwavering support of our title sponsors, Ritchey Logic and Velocio Apparel. As a dynamic and ambitious team, we embody the essence of speed, precision, and style, mirroring the values of our esteemed sponsors.
+            </p>
+            <h2 className='text-lg font-bold m-4'>
+                Our Team: Powered by Passion
+            </h2>
+            <p className='text-base'>
+                Ritchey Velocio Racing is more than just a cycling team; we are a collective of passionate athletes, skilled technicians, and dedicated support staff united by a common goal  achieving excellence in the world of professional cycling. Our roster of elite cyclists represents a diverse array of talents and backgrounds, coming together to form a formidable force on the race circuit. With a shared commitment to hard work, perseverance, and sportsmanship, we proudly carry the Ritchey Velocio Racing banner across the globe.
+            </p>
+            <h2 className='text-lg font-bold m-4'>
+                Join the Journey
+            </h2>
+            <p className='text-base'>
+                Whether you're a cycling enthusiast, a fan of the sport, or a potential partner seeking to align with a winning team, we invite you to join us on this thrilling journey. Ritchey Velocio Racing is not just a team; it's a community of individuals dedicated to pushing the boundaries of what is possible in the world of competitive cycling. Together with Ritchey Logic and Velocio Apparel, we are shaping the future of the sport, one race at a time.
+            </p>
+            <h3 className='text-lg font-semibold mt-4'>
+                Thank you for being part of the Ritchey Velocio Racing experience. Saddle up, and let's ride to victory!
+            </h3>
         </div>
     )
 }
