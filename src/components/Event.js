2a95d73ddfa88e8c6395529826dde482e71a2a0c
samphillips123 <sphillips123@gmail.com>
2023-12-15 22:22:59 -0800

    updated app name

--- src/components/Event.js
+++ src/components/Event.js
@@ -3,7 +3,7 @@ const Event = ({ event }) => {
         <div className='mt-5 border-t-2 border-black'>
             <div className='m-5 text-xl hover:cursor-pointer'>
                 <a href={event.eventUrl} target='_blank'>
-                    <span>{event.eventDate} {event.eventName} - {event.eventLocation}</span>
+                    <span>{event.eventDate} - {event.eventName} - {event.eventLocation}</span>
                 </a>
             </div>
         </div>
